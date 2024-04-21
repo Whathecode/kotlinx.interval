4f04b3f7b6e0f3735e0c8d16e40e327437f5d05a
Whathecode <steven.jeuris@gmail.com>
2024-04-21 20:16:35 +0200

    Build: bump to version 1.0.0-alpha.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ if (publishPropertiesFile.exists()) {
     publishProperties.load(java.io.FileInputStream(publishPropertiesFile))
 }
 group = "io.github.whathecode.kotlinx.interval"
-version = "1.0.0-alpha.4"
+version = "1.0.0-alpha.5"
 nexusPublishing {
     repositories {
         sonatype {
