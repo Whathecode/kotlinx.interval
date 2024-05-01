bf74bdc62d125e3a1dc6e24c4591d625db01e14e
Steven Jeuris <steven.jeuris@gmail.com>
2024-05-01 18:27:54 +0200

    Add `TypeOperations.spacing`

--- kotlinx.interval.datetime/src/commonMain/kotlin/DateTimeTypeOperations.kt
+++ kotlinx.interval.datetime/src/commonMain/kotlin/DateTimeTypeOperations.kt
@@ -11,6 +11,7 @@ internal object InstantOperations : TypeOperations<Instant>
     override val additiveIdentity: Instant = Instant.fromEpochMilliseconds( 0 )
     override val minValue: Instant = Instant.fromEpochSeconds( Long.MIN_VALUE, Long.MIN_VALUE )
     override val maxValue: Instant = Instant.fromEpochSeconds( Long.MAX_VALUE, Long.MAX_VALUE )
+    override val spacing: Instant? = null
 
     override fun unsafeAdd( a: Instant, b: Instant ): Instant = Instant.fromEpochSeconds(
         a.epochSeconds + b.epochSeconds,
@@ -31,6 +32,7 @@ internal object DurationOperations : TypeOperations<Duration>
     private const val MAX_MILLIS = Long.MAX_VALUE / 2
     override val minValue: Duration = -MAX_MILLIS.milliseconds
     override val maxValue: Duration = MAX_MILLIS.milliseconds
+    override val spacing: Duration? = null
 
     override fun unsafeAdd( a: Duration, b: Duration ): Duration = a + b
     override fun unsafeSubtract( a: Duration, b: Duration ): Duration = a - b
