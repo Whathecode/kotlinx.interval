ce0c7095b2d4ac6a0f397e382f5f7a52b2ba0ee3
Steven Jeuris <steven.jeuris@gmail.com>
2024-08-06 21:24:58 +0200

    Doc: overview of supported operations

--- kotlinx.interval/src/commonTest/kotlin/Readme.kt
+++ kotlinx.interval/src/commonTest/kotlin/Readme.kt
@@ -15,4 +15,15 @@ class Readme
         val areExcluded = 10 !in interval && 15 !in interval // true
         val size: UInt = interval.size // 10
     }
+
+    @Test
+    fun introduction_chaining_operations_example()
+    {
+        val start = interval( 0, 100 ) // Interval: [0, 100]
+        val areIncluded = 50 in start && 100 in start // true
+        val splitInTwo = start - interval( 25, 85 ) // Union: [[0, 25), (85, 100]]
+        val areExcluded = 50 !in splitInTwo && 85 !in splitInTwo // true
+        val unite = splitInTwo + interval( 10, 90 ) // Interval: [0, 100]
+        val backToStart = start == unite // true
+    }
 }
